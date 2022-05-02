55c36d4fc9bb0f5f522c7d8692727224325d27b8
williamyalmeida <willdevfron@gmail.com>
2022-05-01 23:49:49 -0300

    provavelmente terminando js

---
+++ src/js/script.js
@@ -0,0 +1,51 @@
+let botaoDeConverter = document.getElementById('botao-de-conversao')
+let trocarMoedaEmselecao = document.getElementById('selecao-moeda')
+
+
+async function conversaoDeMoeda() {
+
+    let moedasApi = await fetch('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL').then(function (respostaMoeda) {
+        return respostaMoeda.json()
+    })
+
+    let dolarDaApi = moedasApi.USDBRL.high
+    let euroDaApi = moedasApi.EURBRL.high
+
+
+    let realBrasileiroValor = Number(document.getElementById('moeda-real-brasileiro').value)
+    let valorDoRealTrocar = document.getElementById('moeda-valor-do-real')
+    let moedaMudarValor = document.getElementById('moedas-valores')
+
+    if (trocarMoedaEmselecao.value === 'Dólar Americano') {
+        let valorDaMoedaEmDolar = realBrasileiroValor / dolarDaApi
+
+        moedaMudarValor.innerHTML = valorDaMoedaEmDolar.toLocaleString('en-us', { style: 'currency', currency: 'USD' })
+    }
+
+    if (trocarMoedaEmselecao.value === 'Euro') {
+        let valorDaMoedaEuro = realBrasileiroValor / euroDaApi
+        moedaMudarValor.innerHTML = valorDaMoedaEuro.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' })
+    }
+
+    valorDoRealTrocar.innerHTML = realBrasileiroValor.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
+}
+
+function trocarMoeda() {
+    let bandeirasTrocar = document.querySelector('#bandeiras')
+    
+    switch(trocarMoedaEmselecao.value) {
+        case'Dólar Americano':
+        bandeirasTrocar.setAttribute('src','src/img/bandeira-eua.svg')
+        break
+        case'Euro':
+        bandeirasTrocar.setAttribute('src','src/img/bandeira-euro.svg')
+        break
+    }
+
+    conversaoDeMoeda()
+    
+}
+
+
+botaoDeConverter.addEventListener('click',conversaoDeMoeda)
+trocarMoedaEmselecao.addEventListener('click',trocarMoeda)
